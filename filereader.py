b1d1e4a04323513559a7ff29648c70896e1447af
gauravsaini964 <gauravsaini964@gmail.com>
2017-08-03 20:34:28 +0530

    Refactor

--- filereader.py
+++ filereader.py
@@ -1,30 +1,29 @@
 from collections import Counter
 import pprint
 x = raw_input('Enter filename with extension.')
+
+
+
 # with open(x) as f:
 #     lines = f.read().splitlines()
 #     # lines = [line.rstrip('\n') for line in open('filename')]
 #     # print lines
 #
 # print lines.count()
-question_mark = {'?'}
-question_count = 0
-chat_dict = {}
-with open(x, 'r') as f:
-    for line in f:
-        splitLine = line.split()
-        chat_dict[(splitLine[0])] = " ".join(splitLine[1:])
-        question_count = Counter(c for line in f for c in line if c in question_mark)
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(chat_dict)
+def total_question_asked():
+    question_mark = {'?'}
+    question_count = 0
+    chat_dict = {}
+    with open(x, 'r') as f:
+        for line in f:
+            splitLine = line.split()
+            question_count = Counter(c for line in f for c in line if c in question_mark)
 
-# count = Counter(chat_dict)
-# pp.pprint(count)
+    print 'Total question asked in this chat: %s' % (question_count.values())
 
-# for i in chat_dict.values():
-#     for j in range(0,len(chat_dict.values())):
-#         if chat_dict.values() == "?":
-#             question_count = question_count + 1
 
-print 'Total question asked in this chat: %s' %(question_count.values())
+def most_talkative():
+
+
+total_question_asked()
