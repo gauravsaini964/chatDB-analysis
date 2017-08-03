a8d96ac7942c4d6f86f53769afb06b8b13494e64
gauravsaini964 <gauravsaini964@gmail.com>
2017-08-03 20:36:42 +0530

    Remove unnecessary code

--- filereader.py
+++ filereader.py
@@ -3,27 +3,18 @@ import pprint
 x = raw_input('Enter filename with extension.')
 
 
-
-# with open(x) as f:
-#     lines = f.read().splitlines()
-#     # lines = [line.rstrip('\n') for line in open('filename')]
-#     # print lines
-#
-# print lines.count()
-
 def total_question_asked():
     question_mark = {'?'}
     question_count = 0
     chat_dict = {}
     with open(x, 'r') as f:
         for line in f:
-            splitLine = line.split()
             question_count = Counter(c for line in f for c in line if c in question_mark)
 
     print 'Total question asked in this chat: %s' % (question_count.values())
 
 
-def most_talkative():
+# def most_talkative():
 
 
 total_question_asked()
