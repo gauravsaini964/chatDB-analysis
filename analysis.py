d9f315029425ff3c2c84f38ab036a39cc4c57ada
gauravsaini964 <gauravsaini964@gmail.com>
2017-08-04 13:50:27 +0530

    Feature : Feel of a meeting

--- analysis.py
+++ analysis.py
@@ -4,6 +4,8 @@ from fileparser import NAME_MSG, MSG_ONLY, x ,name_msg_filgen, message_only_file
 import urllib
 from wordcloud import WordCloud
 import matplotlib.pyplot as plot
+import requests
+import json
 
 # generate files
 name_msg_filgen(x)
@@ -12,6 +14,7 @@ message_only_filegen(x)
 Students = []
 MESSAGE_DICT = []
 d = defaultdict(list)
+
 
 
 def total_question_asked():
@@ -25,54 +28,14 @@ def total_question_asked():
     print 'Total question asked in this chat: %s' % (question_count.values())
 
 
-def most_talkative():
-    chat_dict = {}
-    temp_dict = {}
-    #strip timestamp
-    #make dict as key:value name:chat
 
-    with open(x) as f:
-        lines = f.readlines()
-    with open(x, 'rb') as f:
-        for line in f:
-            splitLine = line.split()
-            temp_dict[(splitLine[0])] = " ".join(splitLine[2:])
-            # Dirty hack to remove timestamp
-            temp_array = temp_dict.values()
-            chat_dict = dict(s.split(':')[:2] for s in temp_array)
-
-            ##Dekh yeh jo upar vala loop hai m.txt ke liye sirf 20 tak kaam kar rha hai
-            #and x.txt ke liye direct show karta hai ki out of index
-    #         mera topic hai ki text analysis like most talkative ?
-    #  most question asked etc. abb isko name:chat ka key:value pair ban jaaye toh saaare
-    #  saare operation asaan ho jayenge
-
-
-    name_occurence_counter = Counter(chat_dict.keys())
-    # pp.pprint(chat_dict)
-
-# def createDictionary(filename):
-#     data_dict = {}
-#     file = open(filename)
-#     contents = file.read()
-#     # print contentsdata_list
-#     data_list = [lines.split(",") for lines in contents.split("\n")]
-#     # pp.pprint(data_list)
-#     for line in data_list:
-#         name = line[0]
-#         message = line[0:]
-#         details = (message)
-#         data_dict[name] = details
-#     pp.pprint(data_dict)
-#     # print data_dict, "\n"
-#     # print data_dict.items(), "\n"
-#     # print data_dict.values()
 
 def createDictionary(filename):
     k,v = 0,0
     with open(filename) as f:
         for line in f:
             if ":" in line:
+                #splits element when it encounters :
                 k, v = line.rstrip().split(":")
                 d[k].extend(map(str.strip, v.split(",")) if v.strip() else [])
             else:
@@ -84,7 +47,29 @@ def createDictionary(filename):
     print 'Student present today : %s' %(d.keys())
 
 
-
+def analyze_Sentiment(filename):
+    # parse each statement in paralleldots
+    # store result in list
+    # count positive and negative
+    # show whichever is max
+    positivity = 0
+    negativity = 0
+    with open(filename) as f:
+        data = f.read().splitlines()
+        for line in range(0,len(data)):
+            sentence = data[line]
+            print sentence
+            API_KEY = 'QcnFyRDYtKrD6Bz68BuKxE8wcCmtBd7cc7DBfJxDf2s'
+            request_url = ('https://apis.paralleldots.com/sentiment?sentence1=%s&apikey=%s') % (sentence, API_KEY)
+            result = requests.get(request_url).json()
+            if result['sentiment'] >= 0.600000:
+                positivity = positivity + 1
+            elif result['sentiment'] < 0.600000:
+                 negativity = negativity + 1
+    if positivity > negativity:
+        print "Overall nature of meeting today was : Positive"
+    else:
+           print "Overall nature of meeting today was : Negative"
 
 
 
@@ -102,6 +87,6 @@ def generate_wordcloud(filename):
     plot.show()
 
 
-# createDictionary(NAME_MSG)
-
-generate_wordcloud(MSG_ONLY)
+# # createDictionary(NAME_MSG)
+# generate_wordcloud(MSG_ONLY)
+analyze_Sentiment(MSG_ONLY)
