8dab3c37b58afa657df092393283f5405f6c2892
gauravsaini964 <gauravsaini964@gmail.com>
2017-08-04 15:16:59 +0530

    Feature : No. of msg

--- analysis.py
+++ analysis.py
@@ -26,7 +26,7 @@ def total_question_asked():
     print colored('Total question asked in this chat: %s', 'cyan', attrs=['bold']) %(question_count.values())
 
 
-def createDictionary(filename):
+def most_talkative(filename):
     k,v = 0,0
     with open(filename) as f:
         for line in f:
@@ -39,9 +39,16 @@ def createDictionary(filename):
 
             Students.append(d.keys())
 
+        # msg_record = []
+        # key_field = key
+        # value_field = str(len([item for item in value]))
+        # fields = key_field + value_field
+        # msg_record.append(fields)
+        # for i in msg_record.values():
     print colored("Total Student present in today's chat : %d", 'cyan', attrs=['bold']) %(len(d.keys()))
-    print colored('Student present today :', 'red', attrs=['bold'])
-    pp(d.keys())
+    print colored('Students Present along with their no of message :', 'white', attrs=['bold'])
+    for key, value in d.items():
+        print (key, len([item for item in value]))
 
 
 def analyze_Sentiment(filename):
@@ -53,9 +60,12 @@ def analyze_Sentiment(filename):
     negativity = 0
     with open(filename) as f:
         data = f.read().splitlines()
+        print colored('Analyzing./', 'white', attrs=['bold'])
+        print colored('This might take time according to file size', 'red', attrs=['bold'])
+        print colored('Analyzing./', 'white', attrs=['bold'])
+        
         for line in range(0,len(data)):
             sentence = data[line]
-            print colored('Analyzing./', 'white',attrs=['bold'])
             API_KEY = 'QcnFyRDYtKrD6Bz68BuKxE8wcCmtBd7cc7DBfJxDf2s'
             request_url = ('https://apis.paralleldots.com/sentiment?sentence1=%s&apikey=%s') % (sentence, API_KEY)
             result = requests.get(request_url).json()
@@ -84,8 +94,7 @@ def generate_wordcloud(filename):
 
 
 
-
 analyze_Sentiment(MSG_ONLY)
 total_question_asked()
-createDictionary(NAME_MSG)
+most_talkative(NAME_MSG)
 generate_wordcloud(MSG_ONLY)
